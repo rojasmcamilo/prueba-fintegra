fa3aabc1fbdbbd48ad997c6f1e6934a82700966d
Camilo Rojas <cristhianrojasm@gmail.com>
2022-10-08 12:39:17 -0500

    Push para subir detalles al repositorio. Aun falta dar funionalidad con multiple nombres. Crsithian Rojas

--- src/components/AgePredictor.js
+++ src/components/AgePredictor.js
@@ -6,20 +6,21 @@ import axios from 'axios';
 
 function AgePredictor () {
 
-    const [ agePrediction, setAgePrediction] = useState('')
+    const [ agePrediction, setAgePrediction] = useState([])
 
+    
 
     const Calcular = (e) => {
         e.preventDefault();
         
-        const name = e.target.name.value;
-
+        const name = [{name: 'laura'},{name: 'laura'},{name: 'laura'}]
         const endPoint = `https://api.agify.io?name=${name}`
         axios.get(endPoint)
         .then(res => {
             const prediction = res.data
             console.log(prediction);
             setAgePrediction(prediction)
+            console.log(agePrediction);
         })
         .catch(error => {
             alert('error, vuelve a intentar')
@@ -29,20 +30,26 @@ function AgePredictor () {
     return(
         <>
         <h1>Predictor de Edad</h1>
-        
-        <form onSubmit={Calcular}>
-            <label id='name'>Ingresa tu nombre</label>
-            <input type='text' name='name'/>
+        <p>Este predictor te permite ingresar uno o varios nombres para calcular la edad del nombre. <br/> Tambien puedes incluir el país.</p>
+
+
+        <form onSubmit={Calcular} className='prediction-form-container'>
+            <label id='name' >Ingresa tu nombre</label>
             <br/>
-            <label id='country'>Selecciona tu pais</label>
+            <input type='text' name='name' required minLength='3'/>
+            <br/>
+            <label id='country'>Selecciona tu pais <span>(opcional)</span> </label>
+            <br/>
             <input type='select' name='country'></input>
             <br/>
             <button type='submit'>Calcular</button>
         </form>
 
         <h2>Resultados</h2>
-        <p>Nombre:{agePrediction.name} </p>
-        <p>Edad:{agePrediction.age} </p>
+        
+        
+
+        
 
         </>
     )    
