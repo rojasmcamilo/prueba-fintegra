68a5b8f8e7be0c2036e4d2e0d622ce5fd510eafd
Camilo Rojas <cristhianrojasm@gmail.com>
2022-10-09 10:52:28 -0500

    Se integra fuincionalidad para calcular el valor de uno o mas nombres. Pendiente integrar opopcion de localizacion. Cristhian Rojas

--- src/components/AgePredictor.js
+++ src/components/AgePredictor.js
@@ -1,4 +1,6 @@
 import "../css/AgePredictor.css"
+import { countryList } from "../data";
+
 
 //Hooks
 import {  useState } from 'react'
@@ -6,51 +8,77 @@ import axios from 'axios';
 
 function AgePredictor () {
 
-    const [ agePrediction, setAgePrediction] = useState([])
-
-    
+    const [agePrediction, setAgePrediction] = useState([])
 
     const Calcular = (e) => {
         e.preventDefault();
+
+        const nameInput = e.target.name.value.trim().split(" ")
+        const country =e.target.country.value
+
+        console.log(country);
+
+        let params = nameInput.map(( person , idx ) =>{
+            if(nameInput.length === 1) {
+                return "name=" + person
+            } else {
+                return "name[]=" + person   
+             }
+        })
+
+        if(params.length > 1 ){
+            params = params.join("&")
+        } else if (params.length === 1) {
+            params = params.toString()
+        }
         
-        const name = [{name: 'laura'},{name: 'laura'},{name: 'laura'}]
-        const endPoint = `https://api.agify.io?name=${name}`
+        const endPoint = `https://api.agify.io/?${params}`
         axios.get(endPoint)
         .then(res => {
             const prediction = res.data
-            console.log(prediction);
             setAgePrediction(prediction)
-            console.log(agePrediction);
         })
         .catch(error => {
             alert('error, vuelve a intentar')
         } )
     }
-    
+
+    console.log(agePrediction);
+
     return(
         <>
         <h1>Predictor de Edad</h1>
         <p>Este predictor te permite ingresar uno o varios nombres para calcular la edad del nombre. <br/> Tambien puedes incluir el país.</p>
 
-
         <form onSubmit={Calcular} className='prediction-form-container'>
-            <label id='name' >Ingresa tu nombre</label>
+            <label id='name'>Nombre(s)</label>
             <br/>
-            <input type='text' name='name' required minLength='3'/>
+            <input type='text' name='name' required minLength='3' />
             <br/>
             <label id='country'>Selecciona tu pais <span>(opcional)</span> </label>
             <br/>
-            <input type='select' name='country'></input>
+            <select type='select' name='country'>
+
+            <option value="">--Please choose an option--</option>
+            {
+                countryList.map((country, idx) => {
+                   return (
+                    <option name='country' value={country.code} key={idx}>{country.name}</option>
+                   )
+                    
+                })
+            }
+
+            </select>
+
+            
             <br/>
             <button type='submit'>Calcular</button>
         </form>
 
         <h2>Resultados</h2>
         
-        
-
-        
-
+       
         </>
     )    
 }
